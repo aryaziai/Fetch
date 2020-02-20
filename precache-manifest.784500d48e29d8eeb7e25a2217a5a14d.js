403b226bae1bd1a8d08036ef960fb6ec2f6efeec
aryaziai <arya721@gmail.com>
2020-02-20 11:41:53 -0800

    Updates

---
+++ precache-manifest.784500d48e29d8eeb7e25a2217a5a14d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e68fc48dc61f3c1c199796818dce7ae",
+    "url": "/Fetch-Frontend/index.html"
+  },
+  {
+    "revision": "c80d4b4bed70e5b58bc4",
+    "url": "/Fetch-Frontend/static/css/main.4c9ac21b.chunk.css"
+  },
+  {
+    "revision": "3e1f7978f54959c76518",
+    "url": "/Fetch-Frontend/static/js/2.05b94482.chunk.js"
+  },
+  {
+    "revision": "86f96943d6537349861c4002a14026e3",
+    "url": "/Fetch-Frontend/static/js/2.05b94482.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c80d4b4bed70e5b58bc4",
+    "url": "/Fetch-Frontend/static/js/main.3e7deae6.chunk.js"
+  },
+  {
+    "revision": "7e64681a3b294899cd28",
+    "url": "/Fetch-Frontend/static/js/runtime-main.966de2ec.js"
+  }
+]);
