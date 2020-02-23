db2a4031098093473558b99dda559368ac45d7b8
aryaziai <arya721@gmail.com>
2020-02-22 19:58:17 -0800

    Updates

--- precache-manifest.a5f894e557b8cd19a1da6196f404840f.js
+++ precache-manifest.a5f894e557b8cd19a1da6196f404840f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "528662447657c6c0c0562fd8cdd1e467",
+    "revision": "b00b244a7798c769a337e21f9199c69e",
     "url": "/Fetch-Frontend/index.html"
   },
   {
-    "revision": "6b1a4bd4fdd9f14f8e30",
+    "revision": "854b070f560b7fc90a1a",
     "url": "/Fetch-Frontend/static/css/main.56fd8d5c.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Fetch-Frontend/static/js/2.74de28e8.chunk.js.LICENSE"
   },
   {
-    "revision": "6b1a4bd4fdd9f14f8e30",
-    "url": "/Fetch-Frontend/static/js/main.6d246689.chunk.js"
+    "revision": "854b070f560b7fc90a1a",
+    "url": "/Fetch-Frontend/static/js/main.e54b554c.chunk.js"
   },
   {
     "revision": "7e64681a3b294899cd28",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Fetch-Frontend/precache-manifest.9d7600b35c89f829fedf8b1a36a5c81a.js"
+  "/Fetch-Frontend/precache-manifest.a5f894e557b8cd19a1da6196f404840f.js"
 );
 
 self.addEventListener('message', (event) => {
