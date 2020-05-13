88d1e2b0b2a4856fda4eef61e1408cc3ee3f1e71
aryaziai <arya721@gmail.com>
2020-05-13 11:53:42 -0700

    css minify

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./App.css";
+import "./App-min.css";
 import Welcome from "./components/Welcome";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
