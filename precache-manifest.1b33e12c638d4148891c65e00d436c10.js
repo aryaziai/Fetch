f404b20d6199251642108762fc11a8cab3b96161
aryaziai <arya721@gmail.com>
2020-06-16 17:01:18 -0700

    Updates

---
+++ precache-manifest.1b33e12c638d4148891c65e00d436c10.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c31199c57a671bb98f6a5b296b77f465",
+    "url": "/Fetch/index.html"
+  },
+  {
+    "revision": "816d73f6bb2d56554786",
+    "url": "/Fetch/static/css/2.183aa86f.chunk.css"
+  },
+  {
+    "revision": "5de079b790ff5513b497",
+    "url": "/Fetch/static/css/main.8f032115.chunk.css"
+  },
+  {
+    "revision": "816d73f6bb2d56554786",
+    "url": "/Fetch/static/js/2.4adac771.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/Fetch/static/js/2.4adac771.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5de079b790ff5513b497",
+    "url": "/Fetch/static/js/main.54d989cc.chunk.js"
+  },
+  {
+    "revision": "fa3789278dc70a463832",
+    "url": "/Fetch/static/js/runtime-main.60603063.js"
+  },
+  {
+    "revision": "87348653c5ef934737b50811fae6ab97",
+    "url": "/Fetch/static/media/hero_1.87348653.png"
+  }
+]);
